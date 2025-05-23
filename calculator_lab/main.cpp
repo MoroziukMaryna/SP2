f031b65ed54fd46be24768e6dbfea9c94f006bf8
Maryna Moroziuk <marynam60@gmail.com>
2025-05-23 12:41:04 +0300

    Added source code, CMakeLists, Makefile and Ninja scripts

---
+++ calculator_lab/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "calculator.h"
+
+int main() 
+{
+    std::cout << "2 + 3 = " << add(2, 3) << std::endl;
+    std::cout << "5 - 1 = " << subtract(5, 1) << std::endl;
+    return 0;
+}
